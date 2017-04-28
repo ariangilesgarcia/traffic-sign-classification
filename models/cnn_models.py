1b9c5038a1c85814fb34519bf3e579a9d593168d
Arian Giles García <arian2822@gmail.com>
2017-04-28 12:49:17 -0300

    Progress

---
+++ models/cnn_models.py
@@ -0,0 +1,59 @@
+import keras
+from keras.layers import Dense
+from keras.layers import Conv2D
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.models import Sequential
+from keras.layers import MaxPooling2D
+from keras.layers import ZeroPadding2D
+
+IMG_SIZE = 64
+IMG_SIZE = 64
+IMG_DEPTH = 3
+
+NUM_CLASSES = 9
+
+def model_1(weights_path=None):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, IMG_DEPTH)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+        return model
+
+
+def model_2(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(IMG_SIZE, IMG_SIZE, IMG_DEPTH)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
