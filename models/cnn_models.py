a9aae392972c735bf761dd5195cf546be04703d5
Arian Giles García <arian2822@gmail.com>
2017-05-01 17:27:24 -0300

    Modified models

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -12,11 +12,6 @@ IMG_SIZE = 64
 IMG_DEPTH = 3
 NUM_CLASSES = 9
 
-NUM_CLASSES_NONE = 10
-
-IMG_SIZE_GERMAN = 32
-IMG_DEPTH_GERMAN = 3
-NUM_CLASSES_GERMAN = 43
 
 def model_1(weights_path=None):
     model = Sequential()
@@ -143,44 +138,7 @@ def model_4(weights_path=None):
     model.add(Dropout(0.5))
     model.add(Dense(1024, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(NUM_CLASSES_NONE, activation='softmax', name='predictions'))
-
-    # Load weights if specified
-    if weights_path:
-        model.load_weights(weights_path)
-
-    return model
-
-
-def model_3_german(weights_path=None):
-    model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(IMG_SIZE_GERMAN, IMG_SIZE_GERMAN, IMG_DEPTH_GERMAN)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-    model.add(Dropout(0.2))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-    model.add(Dropout(0.2))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-    model.add(Dropout(0.2))
-
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(NUM_CLASSES_GERMAN, activation='softmax', name='predictions'))
+    model.add(Dense(NUM_CLASSES, activation='sigmoid', name='predictions'))
 
     # Load weights if specified
     if weights_path:
